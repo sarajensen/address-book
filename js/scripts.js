906839103769c8944341c283d7fd933ff2519e0f
Sara Jensen <sara.jensen@idealist.org>
2016-08-22 22:43:31 -0700

    refactor

--- js/scripts.js
+++ js/scripts.js
@@ -5,34 +5,46 @@ function Contact(first, last) {
   this.addresses = [];
 }
 
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
 function Address(street, city, state) {
   this.street = street;
   this.city = city;
   this.state = state;
 }
 
-Contact.prototype.fullName = function() {
-  return this.firstName + " " + this.lastName;
+Address.prototype.fullAddress = function() {
+  return this.street + ", " + this.city + ", " + this.state;
 }
 
 // user interface logic
 $(document).ready(function() {
   $("#add-address").click(function() {
       $("#new-addresses").append('<div class="new-address">' +
-                                   '<div class="form-group">' +
-                                     '<label for="new-street">Street</label>' +
-                                     '<input type="text" class="form-control new-street">' +
-                                   '</div>' +
-                                   '<div class="form-group">' +
-                                     '<label for="new-city">City</label>' +
-                                     '<input type="text" class="form-control new-city">' +
-                                   '</div>' +
-                                   '<div class="form-group">' +
-                                     '<label for="new-state">State</label>' +
-                                     '<input type="text" class="form-control new-state">' +
-                                   '</div>' +
-                                 '</div>');
-    });
+         '<div class="form-group">' +
+           '<label for="new-street">Street</label>' +
+           '<input type="text" class="form-control new-street">' +
+         '</div>' +
+         '<div class="form-group">' +
+           '<label for="new-city">City</label>' +
+           '<input type="text" class="form-control new-city">' +
+         '</div>' +
+         '<div class="form-group">' +
+           '<label for="new-state">State</label>' +
+           '<input type="text" class="form-control new-state">' +
+         '</div>' +
+        '</div>');
+      });
+
+  function resetFields() {
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+    $("input.new-street").val("");
+    $("input.new-city").val("");
+    $("input.new-state").val("");
+  }
 
   $("form#new-contact").submit(function(event) {
     event.preventDefault();
@@ -59,11 +71,9 @@ $(document).ready(function() {
       $(".last-name").text(newContact.lastName);
       $("ul#addresses").text("");
       newContact.addresses.forEach(function(address) {
-        $("ul#addresses").append("<li>" + address.street + ", " + address.city + " " + address.state + "</li>");
+        $("ul#addresses").append("<li>" + address.fullAddress() + "</li>");
       });
     });
-
-    $("input#new-first-name").val("");
-    $("input#new-last-name").val("");
+    resetFields()
   });
 });
